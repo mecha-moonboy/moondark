42a3ae24faf33dbd541b8b9b01e464e7dabaf3ae
mecha_moonboy <ezshack@gmail.com>
2023-12-08 07:23:14 -0500

    Update fire.lua

--- mods/md_fire/fire.lua
+++ mods/md_fire/fire.lua
@@ -4,4 +4,10 @@ md_fire.fire_node_template = {
     visual_scale = 1.0,
     tiles = {},
     color = "#ffffffff",
+    use_texture_alpha = ...,
+    post_effect_color = "#00000000",
+    sunlight_propagates = true,
+    walkable = true,
+    damage_per_second = 1,
+
 }
