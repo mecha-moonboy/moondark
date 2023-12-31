d77de6f3949aa8cb062796813134f4c4d0353c57
mecha_moonboy <ezshack@gmail.com>
2023-12-31 12:18:10 -0500

    Blackboarding

--- mods/md_herbs/nodes.lua
+++ mods/md_herbs/nodes.lua
@@ -1,10 +1,10 @@
 minetest.register_node("md_herbs:brawsterry_bush", {
     description = "brawsterry bush",
     drawtype = "plantlike",
-    tiles = {"(small_plant.png^[multiply:#115511ff)^(small_plant_berries.png^[multiply:#771122ff)"},
+    tiles = {"(small_plant.png^[multiply:#003311ff)^(small_plant_berries.png^[multiply:#770000ff)"},
 
     -- Interaction
-    groups = {hand = 3},
+    groups = {hand = 1},
     walkable = false,
     sunlight_propagates = true,
     move_resistance = 1,
