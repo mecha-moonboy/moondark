880932d56b4ba67f1f12bc81123e0a07f3592862
mecha_moonboy <ezshack@gmail.com>
2024-04-14 12:49:42 -0400

    Began mob branch
    w h a l e

---
+++ mods/md_fauna/init.lua
@@ -0,0 +1,20 @@
+md_template_mod = {}
+local modname = minetest.get_modpath("md_fauna")
+
+
+
+if minetest.get_modpath("md_herbs") then
+
+end
+if minetest.get_modpath("md_fire") then
+
+end
+-- minetest.register_on_generated(function(minp, maxp, blockseed)
+--     -- do things when a map chunk is generated
+-- end)
+
+-- minetest.register_globalstep(function(dtime)
+--      -- do things every frame
+-- end)
+
+--dofile(modname .. "/api.lua")
